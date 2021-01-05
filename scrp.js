5af806edfd8ed9bb5ae19c8c81d535f9289ddf32
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-01-05 19:04:25 +0300

    first commit

---
+++ scrp.js
@@ -0,0 +1,36 @@
+$(document).ready( function (){
+    let navbar = $(".navbar");
+
+$(window).scroll(function (){
+    let oTop = $(".section2").offset().top - window.innnerHeight;
+    if($(window).scrollTop() > oTop){
+        navbar.addClass("sticky");
+    }
+    else{
+        navbar.removeClass("sticky");
+    }
+});
+
+let nCount = function(selector){
+    $(selector).each(function (){
+        $(this).animate({
+            Counter: $(this).text()
+        },{
+            duration: 4000,
+            easing: "swing",
+            step: function(value){
+                $(this).text(Math.cell(value));
+            }
+        });
+    });
+};
+
+let a = 0;
+$(window).scroll(function (){
+    let oTop = $(".numbers").offset().top - window.innnerHeight;
+    if( a == 0 & $(window).scrollTop()>= oTop){
+        a++;
+        nCount(".rect>h1");
+    }
+});
+});
